684f3e440c7275ad937b9300bb6da16710a59ef7
ryan <ryan-polley@uiowa.edu>
2019-05-01 18:00:41 -0500

    add code to convert text from roman to modern style

--- scraper.py
+++ scraper.py
@@ -4,6 +4,7 @@ from contextlib import closing
 from bs4 import BeautifulSoup
 from time import sleep
 from os.path import isfile
+from parse import *
 
 def simple_get(url):
     try:
@@ -45,4 +46,44 @@ def get_html_from_url(url):
     tree_html = BeautifulSoup(raw_html, 'html.parser')
     return tree_html
     
-
+"""
+convert ancient roman style latin text to modern style latin text
+"""
+def roman_to_modern(text):
+    #the romans wrote in all caps, but we don't want that
+    text = text.lower()
+    #some word lists provide a declension number affixed to the word, remove that
+    #since we're scraping the principle parts from the dictionary instead
+    text = parse("{word:l}{}",text)["word"]
+    #the romans used the letter v to write both u and v, but the dictionaries we're using dont
+    #thankfully we can convert between the two based on some linquistic rules
+    
+    #first add in some word boundary symbols
+    text = list("#"+text+"#")
+    
+    vowels = "aeiouv"
+    for i in range(1,len(text)-1):
+        prev = text[i-1]
+        curr = text[i]
+        next = text[i+1]
+        consonantal = False
+        
+        if(prev == "#" and next in vowels): # #_V
+            consonantal = True
+        elif(prev in vowels and not next not in vowels): # V_V, V_#
+            consonantal = True
+        elif(prev not in vowels and prev != "#" and next == "#"): # C_#
+            consonantal = True
+        
+        if(curr == "u" and prev == "q" or prev == "c"):
+            consonantal = False
+        
+        #sometimes i's are consonants, but they follow the same rules, so replace them with j
+        if (consonantal and curr == "i"):
+            text[i] = "j"
+        
+        #replace the v's with u's
+        if (not consonantal and curr == "v"):
+            text[i] = "u"
+    return "".join(text[1:-1])
+    
